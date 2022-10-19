829d36a2d6981c89670b291c1e86d5f5e1079181
dsgray <dsgray@gmail.com>
2022-10-19 12:20:13 -0400

    remove env

--- init_setup.sh
+++ init_setup.sh
@@ -1,1 +1,9 @@
 # create environment and install requirements
+echo[$(date)]: "START"
+echo[$(date)]: "Creating conda env with python 3.8" 
+conda create --prefix ./env python=3.8 -y
+echo[$(date)]: "activate env"
+source activate .env
+echo[$(date)]: "installing dev requirements"
+pip install -r requirements_dev.txt
+echo[$(date)]: "END"
