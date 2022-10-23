401ffaa7b15fb062cb63f97023f3f85f9bb547ac
dsgray <dsgray@gmail.com>
2022-10-23 19:17:48 -0400

    "legacy-editable"

--- init_setup.sh
+++ init_setup.sh
@@ -1,9 +1,13 @@
 # create environment and install requirements
 echo [$(date)]: "START"
 echo [$(date)]: "Creating conda env with python 3.8"
+conda init bash
 conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate env"
-source activate ./env
+conda init bash
+# conda activate /Users/davidgray/Documents/Data_Science/Project_Template/env
+conda activate /Users/davidgray/Documents/Data_Science/Project_Template/env
 echo [$(date)]: "intalling dev requirements"
+set SETUPTOOLS_ENABLE_FEATURES="legacy-editable" ; pip install -e .
 pip install -r requirements_dev.txt
 echo [$(date)]: "END"
