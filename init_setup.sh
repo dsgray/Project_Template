225be8e4a03cf2c8ea113bdcc2f8f7ddb9b069cb
dsgray <dsgray@gmail.com>
2022-11-10 10:36:45 -0500

    updated files

--- init_setup.sh
+++ init_setup.sh
@@ -1,21 +1,35 @@
-# create environment and install requirements
+## create environment and install requirements
+#echo [$(date)]: "START"
+#echo [$(date)]: "Creating conda env with python 3.8"
+
+#conda init bash
+#conda create --prefix ./env python=3.8 -y
+
+#echo [$(date)]: "activate env"
+## conda activate /Users/davidgray/Documents/Data_Science/Project_Template/env
+## source ~/anaconda3/etc/profile.d/conda.sh
+
+#conda activate /Users/davidgray/Documents/Data_Science/Project_Template/env
+
+#echo [$(date)]: "intalling dev requirements"
+
+#set SETUPTOOLS_ENABLE_FEATURES="legacy-editable" ; pip install -e
+
+##set SETUPTOOLS_ENABLE_FEATURES="legacy-editable"
+
+#pip install -r requirements_dev.txt
+#echo [$(date)]: "END"
+
 echo [$(date)]: "START"
 echo [$(date)]: "Creating conda env with python 3.8"
 
-conda init bash
-conda create --prefix ./env python=3.8 -y
+conda create --prefix ./ python=3.8 -y
 
 echo [$(date)]: "activate env"
-# conda activate /Users/davidgray/Documents/Data_Science/Project_Template/env
-# source ~/anaconda3/etc/profile.d/conda.sh
-
-conda activate /Users/davidgray/Documents/Data_Science/Project_Template/env
+source activate ./env
 
 echo [$(date)]: "intalling dev requirements"
 
-set SETUPTOOLS_ENABLE_FEATURES="legacy-editable" ; pip install -e
-
-#set SETUPTOOLS_ENABLE_FEATURES="legacy-editable"
-
 pip install -r requirements_dev.txt
+
 echo [$(date)]: "END"
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read
-
 __version__ = "0.0.0"
 
 REPO_NAME = "ProjectTemplate"
@@ -15,7 +14,7 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package",
-    long_description=long_description,
+ #   long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
